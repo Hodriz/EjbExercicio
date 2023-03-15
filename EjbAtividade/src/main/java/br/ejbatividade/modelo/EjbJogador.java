667cc61cfa8efa872710635731f3e2be6428bd35
Rodrigo <Rodrigo@host.docker.internal>
2023-03-15 17:18:56 -0300

    acrescimo da Lista dos Jogadores

---
+++ EjbAtividade/src/main/java/br/ejbatividade/modelo/EjbJogador.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatefulEjbClass.java to edit this template
+ */
+package br.ejbatividade.modelo;
+
+import java.util.ArrayList;
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author Rodrigo
+ */
+@Stateful
+public class EjbJogador {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+       ArrayList<Jogador>listaJogador=new ArrayList<>();
+            
+    Jogador player=new Jogador();
+
+    public Jogador getPlayer() {
+        return player;
+    }
+
+    public void setPlayerNome(Jogador player) {
+        this.player = player;
+    }
+    
+    public void addPlayer(){
+     listaJogador.add(player);
+    }
+    
+    public void showPlayerList(){
+     for(int i=0;i<listaJogador.size();i++){
+         System.out.println(i);
+     }
+    }
+   
+}
---
+++ EjbAtividade/src/main/java/br/ejbatividade/modelo/JsfJogador.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package br.ejbatividade.modelo;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Rodrigo
+ */
+@Named(value = "jsfJogador")
+@SessionScoped
+public class JsfJogador implements Serializable {
+
+    @EJB
+    private EjbJogador ejbJogador;
+
+    /**
+     * Creates a new instance of JsfJogador
+     */
+    public JsfJogador() {
+    }
+    
+    
+    
+      public ArrayList<Jogador>listarJogador(){
+    return ejbJogador.listaJogador;
+    }
+    
+    
+    
+}
